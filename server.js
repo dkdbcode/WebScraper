e0912f0f1e08a3f0a82cf70d85006450a149fb48
David DB <dkdbcode@gmail.com>
2017-07-24 18:40:47 -0700

    [setup] made package.json. [feat] made simple server.

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var app = express()
+var fs = require('fs')
+var request = require('request')
+var cheerio = require('cheerio')
+
+app.get('/webscraper', function(req, res){
+
+    //Future web scraping
+
+})
+
+app.listen('3001')
+
+exports = module.exports = app
